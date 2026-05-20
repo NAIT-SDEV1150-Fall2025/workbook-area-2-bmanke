5a6c626f10983c9893e71dd061cf9e930a5de40f
B Manke <bmanke2@nait.ca>
2026-05-20 16:55:57 -0600

    lesson 5 querySelector

--- lesson-05-starter/src/main.js
+++ lesson-05-starter/src/main.js
@@ -1,6 +1,18 @@
 console.log('Lesson 05 starter loaded');
 
 // 1. Selecting elements
+// Selecting the page title with the document.querySelector using an ID.
+const title = document.querySelector(`#page-title`);
+
+// Selecting the tagline class with the document.querySelector
+const tagline = document.getElementsByClassName(`tagline`);
+
+const heroImg = document.querySelector(`#hero-img`);
+const heroCap = document.querySelector(`#hero-caption`);
+
+const dynaBox = document.querySelector(`#dynamic-box`);
+const footNote = document.getElementById(`footer-note`);
+// document.querySelectorAll is used for class names and tags that creates an array.
 
 // 2. textContent vs innerHTML
 // When you only need text (no markup), prefer textContent:
